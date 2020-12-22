864669ab68cb1de1fc4b9146076a1c146b1047b6
macmiller87 <macamagolf@gmail.com>
2020-12-22 10:42:32 -0300

    Adicionar arquivos de projeto.

---
+++ p11aula/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace p11aula
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDias = new System.Windows.Forms.Label();
+            this.txtDias = new System.Windows.Forms.TextBox();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.lblRes2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btbLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDias.Location = new System.Drawing.Point(12, 163);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(334, 19);
+            this.lblDias.TabIndex = 0;
+            this.lblDias.Text = "Informe A Quantidade De Dias Hospedado:";
+            // 
+            // txtDias
+            // 
+            this.txtDias.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtDias.Location = new System.Drawing.Point(352, 156);
+            this.txtDias.Name = "txtDias";
+            this.txtDias.Size = new System.Drawing.Size(100, 26);
+            this.txtDias.TabIndex = 1;
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRes.Location = new System.Drawing.Point(12, 206);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 19);
+            this.lblRes.TabIndex = 2;
+            // 
+            // lblRes2
+            // 
+            this.lblRes2.AutoSize = true;
+            this.lblRes2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRes2.Location = new System.Drawing.Point(12, 244);
+            this.lblRes2.Name = "lblRes2";
+            this.lblRes2.Size = new System.Drawing.Size(0, 19);
+            this.lblRes2.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.Location = new System.Drawing.Point(531, 284);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(108, 38);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btbLimpar
+            // 
+            this.btbLimpar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btbLimpar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btbLimpar.Location = new System.Drawing.Point(190, 284);
+            this.btbLimpar.Name = "btbLimpar";
+            this.btbLimpar.Size = new System.Drawing.Size(108, 38);
+            this.btbLimpar.TabIndex = 5;
+            this.btbLimpar.Text = "Limpar";
+            this.btbLimpar.UseVisualStyleBackColor = false;
+            this.btbLimpar.Click += new System.EventHandler(this.btbLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Red;
+            this.btnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSair.Location = new System.Drawing.Point(12, 284);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(108, 38);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(85, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 26);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Valor Da Diária Do Hotel R$ 95,00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 80);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "valor da taxa de serviço:\r\n\r\n15% por dia, para diárias menor do que 10,\r\n10% por " +
+    "dia, para 10 dias de diária,\r\n5% por dia, para diárias acima de 10.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(651, 334);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btbLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblRes2);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.txtDias);
+            this.Controls.Add(this.lblDias);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.TextBox txtDias;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Label lblRes2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btbLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ p11aula/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p11aula
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {    
+            int lblDias;
+            double TotalDiarias,TotalPagar,Diaria,
+                TaxaQuinze,TaxaDez,TaxaCinco,
+              TotTaxaQuinze,TotTaxaDez,TotTaxaCinco;
+
+            lblDias = Convert.ToInt32(txtDias.Text);
+            Diaria = 95.00;
+
+            if(lblDias < 10) {
+
+                TotalDiarias = lblDias * Diaria;
+                TaxaQuinze = TotalDiarias * 0.15;
+                TotTaxaQuinze = TaxaQuinze;
+                TotalPagar = TotalDiarias + TotTaxaQuinze;
+                lblRes.Text = ("Foram "+lblDias+" Dias De Hospedagem, O Valor Da Taxa De Serviço É R$ "+TotTaxaQuinze+" Reais!");
+                lblRes2.Text = ("O Total a Ser Pago É R$ " + TotalPagar + " Reais!");
+
+            } else if(lblDias == 10) {
+
+                TotalDiarias = lblDias * Diaria;
+                TaxaDez = TotalDiarias * 0.10;
+                TotTaxaDez = TaxaDez;
+                TotalPagar = TotalDiarias + TotTaxaDez;
+                lblRes.Text = ("Foram "+lblDias+ " Dias De Hospedagem, O Valor Da Taxa De Serviço É R$ "+TotTaxaDez+" Reais!");
+                lblRes2.Text = ("O Total a Ser Pago É R$ " + TotalPagar + " Reais!");
+
+            } else {
+
+                TotalDiarias = lblDias * Diaria;
+                TaxaCinco = TotalDiarias * 0.05;
+                TotTaxaCinco = TaxaCinco;
+                TotalPagar = TotalDiarias + TotTaxaCinco;
+                lblRes.Text = ("Foram "+lblDias+ " Dias De Hospedagem, O Valor Da Taxa De Serviço É R$ "+TotTaxaCinco+" Reais!");
+                lblRes2.Text = ("O Total a Ser Pago É R$ " + TotalPagar + " Reais!");
+            }
+
+        }
+
+        private void btbLimpar_Click(object sender, EventArgs e)
+        {
+            txtDias.Text = " ";
+            lblRes.Text = " ";
+            lblRes2.Text = " ";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
